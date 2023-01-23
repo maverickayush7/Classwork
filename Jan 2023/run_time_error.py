a8c8df4955a6505e544a85212e367163e81567b4
maverickayush7 <0001ayushkumar@gmail.com>
2023-01-23 10:11:23 +0530

    exception - super_class

--- Jan 2023/run_time_error.py
+++ Jan 2023/run_time_error.py
@@ -3,5 +3,21 @@ try:
     print(a)
 except IndexError:
     print("index error")
-except NameError:   #NameError is a class name 
+except NameError:   #NameError is a class name (sub-class)
     print("name error")
+
+# at line 3 "NameError" object is created and then it surf for the same object name in except
+
+
+try:
+    print("hello")
+    # print(a)      # an object of type nameerror will be generated
+
+except Exception:    # super-class  (first match)
+    print("error")
+except IndexError:
+    print("index error")
+except NameError:   #NameError is a class name (sub-class)
+    print("name error")
+else:
+    print("no error")
